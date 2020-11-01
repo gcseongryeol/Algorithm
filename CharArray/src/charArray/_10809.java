b376d842a8d195ebf49909a58725a8c7103f2a94
LeeSeongRyeol <gcsony@naver.com>
2020-11-01 20:00:56 +0900

    Nov 1st Study

---
+++ CharArray/src/charArray/_10809.java
@@ -0,0 +1,20 @@
+//package charArray;
+//
+//import java.util.Scanner;
+//
+//public class _10809 {
+//	
+//	public static void main(String[] args) {
+//		
+//		Scanner scan = new Scanner(System.in);
+//		
+//		String str = scan.nextLine();
+//		scan.close();
+//		
+//		for(char i = (char)97; i < (char)123; i++) {
+//			System.out.print(str.indexOf(i) + " ");
+//		}
+//		
+//	}
+//
+//}
---
+++ CharArray/src/charArray/_11720.java
@@ -0,0 +1,28 @@
+//package charArray;
+//
+//import java.util.Scanner;
+//
+//public class _11720 {
+//	
+//	public static void main(String[] args) {
+//		
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int n = scan.nextInt();
+//		String a = scan.next();
+//		scan.close();
+//		
+//		int sum = 0;
+//		
+//		for(int i = 0; i < n; i++) {
+//			sum += a.charAt(i) - '0';
+//		}
+//		
+//		System.out.println(sum);
+//		
+//		
+//		
+//		
+//	}
+//
+//}
